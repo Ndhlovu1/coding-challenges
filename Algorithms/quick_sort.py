2b8e809befc7310d06b683ba710501eb7357cc6a
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-04-17 00:19:00 +0200

    QuickSort Cases

--- Algorithms/quick_sort.py
+++ Algorithms/quick_sort.py
@@ -1,6 +1,13 @@
 # QuickSort Algorithm
 """ 
-A sorting algorithm that uses DIVIDE & CONQUER method
+A sorting algorithm that uses DIVIDE & CONQUER methodology.
 
+--CASE
+    Given an array of items, a place is determined on the array on which to split and this is called the pivot point.
+    All values GREATER than this point go to the RIGHT
+    All values LESS than this point go to the LEFT - THESE 2 STEPS WILL LEAD TO TWO ARRAYS
+    -
+    The above process is applied to the arrays until there are no more elements left to sort
+    
 """
 
