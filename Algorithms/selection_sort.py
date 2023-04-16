a2a049c31c9d223b879990b41df7ed33dda4696d
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-04-16 22:30:25 +0200

    Selection Sort(ST-A) Time and Space Complexity Definitions

--- Algorithms/selection_sort.py
+++ Algorithms/selection_sort.py
@@ -2,3 +2,21 @@
 1. Take an Array item and iterate from left to right and swap this value with the lowest value found to the right of this item
 2. Repeat this process with every single value in the array until it is sorted
 """
+
+#Considering the Space and the Time Complexity of this Algorithm
+"""
+
+---------------------------------
+SPACE COMPLEXIT = O(1) Auxillary
+---------------------------------
+TIME COMPLEXITY :
+    WORST CASE      = 0(n**2)
+    AVERAGE CASE    = 0(n**2)
+    BEST CASE       = 0(n**2)
+---------------------------------
+
+"""
+
+
+
+
