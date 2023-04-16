50eed565f348aca63da6f48c4abe231985e9bdb4
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-04-16 22:34:20 +0200

    ST-A implementation steps pseuodocode added

--- Algorithms/selection_sort.py
+++ Algorithms/selection_sort.py
@@ -5,7 +5,6 @@
 
 #Considering the Space and the Time Complexity of this Algorithm
 """
-
 ---------------------------------
 SPACE COMPLEXIT = O(1) Auxillary
 ---------------------------------
@@ -15,6 +14,13 @@ TIME COMPLEXITY :
     BEST CASE       = 0(n**2)
 ---------------------------------
 
+"""
+
+# STEPS TO CREATE A SELECTION SORT ALGORITHM(ST-A)
+"""
+1. Find the SMALLEST Value and Swap it with the first place of the array
+2. FInd the second SMALLEST Value and Swap it with second place in the array
+3. Repeat the Above steps until all items are changed and ordered from SMALLEST TO LARGEST
 """
 
 
