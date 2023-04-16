3692388479a60d20a46ab19f6f9f36a0a1b7f306
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-04-16 22:26:54 +0200

    Selection Sort Algorithm Basic Implementation

---
+++ Algorithms/selection_sort.py
@@ -0,0 +1,4 @@
+"""
+1. Take an Array item and iterate from left to right and swap this value with the lowest value found to the right of this item
+2. Repeat this process with every single value in the array until it is sorted
+"""
